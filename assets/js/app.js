6249cc7ed354f371eee205515d24eb164b9348bb
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2023-07-08 00:23:12 +0200

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,51 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 import GesturesPlugin from 'phaser3-rex-plugins/plugins/gestures-plugin.js';
 
-import ball from './../img/ball.png'
-import backgroundSand from './../img/background-sand.jpg'
+import ball from './../img/ball.png';
+import arrow from './../img/arrow.png';
+import backgroundSand from './../img/background-sand.jpg';
 
 class Game extends Phaser.Scene {
 
+	targetingModeIsEnabled = false;
+	cameraIsEnabled = true;
+
+	currentLineTarget;
+
 	preload() {
 		this.load.image('ball', ball);
+		this.load.image('arrow', arrow);
 		this.load.image('background-sand', backgroundSand);
 	}
 
 	create() {
+
+		this.matter.world.setBounds(0, 0, window.innerWidth, window.innerHeight, 32, true, true, false, true);
+		this.matter.world.disableGravity()
 
 		this.updateBackground();
 		this.addBall();
 		this.initPinchZoom();
 	}
 
-	update() {
-	}
-
 	addBall() {
 
-		const image = this.add.image(window.innerWidth / 2, window.innerHeight - 300, 'ball').setInteractive({
-			draggable: true
+		this.ball = this.matter.add.image(100, 100, 'ball');
+
+		this.ball.setCircle();
+		this.ball.setFriction(0.005);
+		this.ball.setBounce(.2);
+		this.ball.setInteractive({ draggable : true });
+
+		this.ball.x = window.innerWidth / 2;
+		this.ball.y = window.innerHeight - 300;
+
+		this.ball.on('pointerdown', () => {
+			this.addTargeting();
 		});
 
-		image.inputEnabled = true;
-		image.fixedToCamera  = true;
-		image.input.priorityID = 1;
-
-		image.on('pointerdown', () => {}, this);
-
-		image.on('drag', (pointer, x, y) => {
-			image.x = x;
-			image.y = y;
+		this.ball.on('drag', (event, x, y) => {
+			this.drawVelocotyIndicator(x, y)
 		});
 	}
 
@@ -43,12 +53,58 @@ class Game extends Phaser.Scene {
 		this.backgroundSand = this.add.tileSprite(window.innerWidth / 2, window.innerHeight / 2, window.innerWidth, window.innerHeight, 'background-sand');
 	}
 
+	addTargeting() {
+
+		if (this.targetingModeIsEnabled) {
+			return;
+		}
+
+		this.targetingModeIsEnabled = true;
+		this.cameraIsEnabled = false;
+
+		// Draw arrow
+		this.arrow = this.add.tileSprite(this.ball.x, this.ball.y-55, 32, 32, 'arrow');
+
+		// Draw velocity indicator
+		this.drawVelocotyIndicator(this.ball.y);
+	}
+
+	drawVelocotyIndicator(x, y) {
+
+		if (this.currentLineTarget) {
+			this.currentLineTarget.destroy();
+		}
+
+		this.currentLineTarget = this.add.graphics();
+
+		this.currentLineTarget.clear();
+
+		if (y <= this.ball.y) {
+			return;
+		}
+
+		this.currentLineTarget.lineStyle(5, 0xff0000);
+		this.currentLineTarget.lineBetween(this.ball.x, this.ball.y, x, y);
+	}
+
+	destroyVelocotyIndicator() {
+
+		this.arrow.destroy();
+		this.currentLineTarget.destroy();
+	}
+
 	initPinchZoom() {
 
 		const pinch = this.rexGestures.add.pinch();
+		const camera = this.cameras.main;
 
-		pinch.on('pinch', pinch => {
-			this.cameras.main.zoom *= pinch.scaleFactor;
+		pinch.on('drag1', pinch => {
+
+			if (!this.cameraIsEnabled) {
+				return;
+			}
+
+			camera.scrollY -= pinch.drag1Vector.y / camera.zoom;
 		});
 	}
 }
@@ -62,6 +118,13 @@ const config = {
 		zoom: Phaser.Scale.MAX_ZOOM,
 		pageAlignHorizontally: true,
 		pageAlignVertically : true
+    },
+	physics: {
+        default: 'matter',
+        matter: {
+            enableSleeping: true,
+			debug: true,
+        }
     },
 	plugins: {
 		scene: [{
