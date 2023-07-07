97b7094a3d0f58d7a96dc5b39fe9d742f0dd816d
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2023-07-07 18:09:34 +0200

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,63 +4,73 @@ import GesturesPlugin from 'phaser3-rex-plugins/plugins/gestures-plugin.js';
 import ball from './../img/ball.png'
 import backgroundSand from './../img/background-sand.jpg'
 
-const config = {
-  type: Phaser.AUTO,
-  width: window.outerWidth,
-  height: window.outerHeight,
-  scene: {
-    preload: preload,
-    create: create
-  },
-  plugins: {
-    scene: [{
-        key: 'rexGestures',
-        plugin: GesturesPlugin,
-        mapping: 'rexGestures'
-    }]
+class Game extends Phaser.Scene {
+
+	preload() {
+		this.load.image('ball', ball);
+		this.load.image('background-sand', backgroundSand);
+	}
+
+	create() {
+
+		this.updateBackground();
+		this.addBall();
+		this.initPinchZoom();
+	}
+
+	update() {
+	}
+
+	addBall() {
+
+		const image = this.add.image(window.innerWidth / 2, window.innerHeight - 300, 'ball').setInteractive({
+			draggable: true
+		});
+
+		image.inputEnabled = true;
+		image.fixedToCamera  = true;
+		image.input.priorityID = 1;
+
+		image.on('pointerdown', () => {}, this);
+
+		image.on('drag', (pointer, x, y) => {
+			image.x = x;
+			image.y = y;
+		});
+	}
+
+	updateBackground() {
+		this.backgroundSand = this.add.tileSprite(window.innerWidth / 2, window.innerHeight / 2, window.innerWidth, window.innerHeight, 'background-sand');
+	}
+
+	initPinchZoom() {
+
+		const pinch = this.rexGestures.add.pinch();
+
+		pinch.on('pinch', pinch => {
+			this.cameras.main.zoom *= pinch.scaleFactor;
+		});
+	}
 }
+
+const config = {
+	type: Phaser.AUTO,
+	width: window.innerWidth,
+	height: window.innerHeight,
+	scale: {
+        mode: Phaser.Scale.SHOW_ALL,
+		zoom: Phaser.Scale.MAX_ZOOM,
+		pageAlignHorizontally: true,
+		pageAlignVertically : true
+    },
+	plugins: {
+		scene: [{
+			key: 'rexGestures',
+			plugin: GesturesPlugin,
+			mapping: 'rexGestures'
+		}]
+	},
+	scene: Game
 };
 
-function preload()
-{
-  this.load.image('ball', ball);
-  this.load.image('background-sand', backgroundSand);
-}
-
-function create()
-{
-  this.add.tileSprite(0, 0, window.outerWidth * 2, window.outerHeight * 2, 'background-sand');
-
-  addBall.apply(this);
-
-  const pinch = this.rexGestures.add.pinch();
-
-  var camera = this.cameras.main;
-    pinch
-        .on('drag1', function (pinch) {
-            var drag1Vector = pinch.drag1Vector;
-            camera.scrollX -= drag1Vector.x / camera.zoom;
-            camera.scrollY -= drag1Vector.y / camera.zoom;
-        })
-        .on('pinch', function (pinch) {
-            var scaleFactor = pinch.scaleFactor;
-            camera.zoom *= scaleFactor;
-        }, this)
-}
-
-function addBall() {
-  
-  const image = this.add.image(window.outerWidth / 2, window.outerHeight - 300, 'ball').setInteractive({ draggable: true });
-
-  image.on('pointerdown', function() {
-    console.log('clicked')
-  }, this);
-  
-  image.on('drag', function (pointer, x, y) {
-      image.x = x;
-      image.y = y;
-  });
-}
-
-
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
