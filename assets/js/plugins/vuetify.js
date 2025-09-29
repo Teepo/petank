f423ff4968e2f6207497d6bb529dcff294eb532c
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2025-09-30 00:42:52 +0200

    update vite

--- assets/js/plugins/vuetify.js
+++ assets/js/plugins/vuetify.js
@@ -2,15 +2,11 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 import colors from 'vuetify/lib/util/colors';
 
 export const vuetify = createVuetify({
-    components,
-    directives,
     theme: {
         defaultTheme: 'light',
         themes: {
